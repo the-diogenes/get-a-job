626793d3cd6c154f49e4703ab26070ce0f0d978c
the-diogenes <the-diogenes@users.noreply.github.com>
2026-05-21 16:27:57 -0700

    Add config.js with Supabase URL + anon key for shared chat/board

---
+++ config.js
@@ -0,0 +1,6 @@
+window.GAJ_CONFIG = {
+  supabaseUrl: "https://owzcdxweqkfgxarokjhr.supabase.co",
+  supabaseAnonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93emNkeHdlcWtmZ3hhcm9ramhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MDUxMDgsImV4cCI6MjA5NDk4MTEwOH0.CqYToraO3jFG2OUw_DoJ1EPZY5PeLvzx3V55Gvssabo",
+  notifyEmail: "john.raymond.jr@gmail.com",
+};
